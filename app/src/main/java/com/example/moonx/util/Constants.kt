897959b77ce11bb26133dec935909c77715ad960
altunoymak <yusuf_altunoymak@hotmail.com>
2023-10-27 23:42:59 +0300

    OkHttp, Interceptor, BaseRepo added.

--- app/src/main/java/com/example/moonx/util/Constants.kt
+++ app/src/main/java/com/example/moonx/util/Constants.kt
@@ -4,9 +4,16 @@ object Constants {
     const val BASE_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
     const val BASE_URL_GPT = "https://api.openai.com/"
     const val API_KEY = "X67WNK3AG8NMS6EZARXR5M4WR"
-    const val BEARER_TOKEN = "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"
+    const val BEARER_TOKEN = "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"
     const val METRIC_UNIT = "metric"
     const val INCLUDE_DAYS = "days"
-
-
+    val dayProperties = listOf(
+        "datetime",
+        "moonphase",
+        "sunrise",
+        "sunset",
+        "moonrise",
+        "moonset",
+        "temp"
+    )
 }
